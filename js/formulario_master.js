a7c5b14b9e1f0353d45841ff3235b4be99a28533
unknown <cyateya35439@universidadean.edu.co>
2019-02-25 00:51:16 -0500

    solucionando formulario

--- js/formulario_master.js
+++ js/formulario_master.js
@@ -16,80 +16,118 @@ $(function() {
 
 });
 
+var myform = $("form#myform");
+var mygeneral = $("form#contactForm");
+var cerrarForm = $('#contact');
+myform.submit(function(event){
+    event.preventDefault();
 
-(function () {
-    emailjs.init("user_p7Lb20gXPbFdHAmEGDXlp");
-})();
-const vue = new Vue({
-    el: '#app',
-    data() {
-        return {
-            from_name: '',
-            from_email: '',
-            message: '',
-            phone: '',
-        }
-    },
-    methods: {
-        enviar() {
-            let data = {
-                from_name: this.from_name,
-                from_email: this.from_email,
-                message: this.message,
-                phone: this.phone,
-            };
+    var params = myform.serializeArray().reduce(function(obj, item) {
+        obj[item.name] = item.value;
+        return obj;
+    }, {});
 
+    // Change to your service ID, or keep using the default service
+    var service_id = "default_service";
 
-            //validar campos del formulario
-            var txtNombre = document.getElementById('nombre').value;
-            var txtCorreo = document.getElementById('correo').value;
-            var txttelefono = document.getElementById('telefono').value;
-            var banderaRBTN = false;
+    var template_id = "template_2WohEOBK";
+    myform.find("button").text("Enviando..");
+    emailjs.send(service_id, template_id, params)
+        .then(function(){
+            alert("Se envio Tu Mensaje.. Gracias");
+            myform.find("button").text("Enviando..");
+            mygeneral.close();
+            myform.close();
+            cerrar();
+            cerrarForm.close();
+        }, function(err) {
+            //alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
+            myform.find("button").text("Enviando..");
+            cerrar();
+            cerrarForm.close();
+        });
 
-            //Test campo obligatorio
-            if(txtNombre == null || txtNombre.length == 0 || /^\s+$/.test(txtNombre)){
-                alert('ERROR: El campo nombre no debe ir vacío o lleno de solamente espacios en blanco');
-                return false;
-            }
-            //Test edad
-            if(txttelefono == null || txttelefono.length == 0 || isNaN(txttelefono)){
-                alert('ERROR: Debe ingresar un Telefono');
-                return false;
-            }
-
-
-            //Test correo
-            if(!(/\S+@\S+\.\S+/.test(txtCorreo))){
-                alert('ERROR: Debe escribir un correo válido');
-                return false;
-            }
-
-
-            if(txtNombre != null && txtCorreo != null && txttelefono != null){
-                emailjs.send("gmail", "template_2WohEOBK", data)
-                    .then(function (response) {
-                        if (response.text === 'OK') {
-                            alert('El correo se ha enviado de forma exitosa');
-                            //$("body").html('<div alignt="center"><h1>Enviado Exitosamente!!!</h1></div>'); //Marca nuevo contenido con un mensaje que se envio exitosamente
-                            document.getElementById('contactForm').close;
-                            //contactForm
-                            setTimeout(function(){
-                                window.close();
-                            },1000); //Dejara un tiempo de 3 seg para que el usuario vea que se envio el formulario correctamente
-
-
-
-                        }
-                        console.log("SUCCESS. status=%d, text=%s", response.status, response.text);
-                    }, function (err) {
-                        alert('Ocurrió un problema al enviar el correo');
-                        console.log("FAILED. error=", err);
-                    });
-            }else{
-                alert('Por favor verifique los campos del formulario');
-            }
-
-        }
-    }
+    return false;
 });
+function cerrar() {
+    window.close();
+}
 
+
+//
+// (function () {
+//     emailjs.init("user_p7Lb20gXPbFdHAmEGDXlp");
+// })();
+// const vue = new Vue({
+//     el: '#app',
+//     data() {
+//         return {
+//             from_name: '',
+//             from_email: '',
+//             message: '',
+//             phone: '',
+//         }
+//     },
+//     methods: {
+//         enviar() {
+//             let data = {
+//                 from_name: this.from_name,
+//                 from_email: this.from_email,
+//                 message: this.message,
+//                 phone: this.phone,
+//             };
+//
+//
+//             //validar campos del formulario
+//             var txtNombre = document.getElementById('nombre').value;
+//             var txtCorreo = document.getElementById('correo').value;
+//             var txttelefono = document.getElementById('telefono').value;
+//             var banderaRBTN = false;
+//
+//             //Test campo obligatorio
+//             if(txtNombre == null || txtNombre.length == 0 || /^\s+$/.test(txtNombre)){
+//                 alert('ERROR: El campo nombre no debe ir vacío o lleno de solamente espacios en blanco');
+//                 return false;
+//             }
+//             //Test edad
+//             if(txttelefono == null || txttelefono.length == 0 || isNaN(txttelefono)){
+//                 alert('ERROR: Debe ingresar un Telefono');
+//                 return false;
+//             }
+//
+//
+//             //Test correo
+//             if(!(/\S+@\S+\.\S+/.test(txtCorreo))){
+//                 alert('ERROR: Debe escribir un correo válido');
+//                 return false;
+//             }
+//
+//
+//             if(txtNombre != null && txtCorreo != null && txttelefono != null){
+//                 emailjs.send("gmail", "template_2WohEOBK", data)
+//                     .then(function (response) {
+//                         if (response.text === 'OK') {
+//                             alert('El correo se ha enviado de forma exitosa');
+//                             //$("body").html('<div alignt="center"><h1>Enviado Exitosamente!!!</h1></div>'); //Marca nuevo contenido con un mensaje que se envio exitosamente
+//                             document.getElementById('contactForm').close;
+//                             //contactForm
+//                             setTimeout(function(){
+//                                 window.close();
+//                             },1000); //Dejara un tiempo de 3 seg para que el usuario vea que se envio el formulario correctamente
+//
+//
+//
+//                         }
+//                         console.log("SUCCESS. status=%d, text=%s", response.status, response.text);
+//                     }, function (err) {
+//                         alert('Ocurrió un problema al enviar el correo');
+//                         console.log("FAILED. error=", err);
+//                     });
+//             }else{
+//                 alert('Por favor verifique los campos del formulario');
+//             }
+//
+//         }
+//     }
+// });
+//
